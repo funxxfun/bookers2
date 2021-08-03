b3e3ae32c987a474224af331b5d32c29954d1b0a
funxxfun <23shirasawa@gmail.com>
2021-08-03 07:39:54 +0000

    [Update]bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
-
   devise_for :users, controllers: {   registrations: 'users/registrations', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get "/home/about" => "homes#about"
-  resources :books
   resources :users
+  resources :books do
+    resources :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
