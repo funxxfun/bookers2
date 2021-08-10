2dee0e40d3d22869dba34b2739583f53487957f1
funxxfun <23shirasawa@gmail.com>
2021-08-10 06:11:48 +0000

    [Finish]bookers2-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,17 +4,19 @@ before_action :current_user, only: [:destroy]
 
 def create
  @book = Book.find(params[:book_id])
- comment = BookComment.new(book_comment_params)
- comment.user_id = current_user.id
- comment.book_id = book.id
- comment.save
+ @comment = BookComment.new(book_comment_params)
+ @comment.user_id = current_user.id
+ @comment.book_id = @book.id
+ @comment.save
+ @new_comment = BookComment.new
  # 非同期通信のために削除
  # redirect_to request.referer
 end
 
 def destroy
  @book = Book.find(params[:book_id])
- BookComment.find_by(id: params[:id],book_id:params[:book_id]).destroy
+ BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
+ @new_comment = BookComment.new
  # 非同期通信のため削除
  # redirect_to request.referer
 end
